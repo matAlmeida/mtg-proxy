23f50687af80392487618087a2de613a3a01842e
Matheus Almeida <mat.almeida@live.com>
2019-07-12 22:35:21 -0300

    feat(create): create PDF

--- src/commands/create.js
+++ src/commands/create.js
@@ -4,6 +4,7 @@ import filesystem from "fs-jetpack";
 import Listr from "listr";
 import chalk from "chalk";
 import Jimp from "jimp";
+import HTML5ToPDF from "html5-to-pdf";
 
 import api from "../services/api";
 
@@ -71,49 +72,48 @@ async function fetchCard({ quantity, cardName }, options) {
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
   const image = await Jimp.read(template);
 
-  image
-    .print(font, 82, 77, cardInfo.fullName)
-    .print(
-      font,
-      82,
-      77,
-      {
-        text: cardInfo.manaCost ? cardInfo.manaCost : "",
-        alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT
-      },
-      640
-    )
-    .print(font, 82, 642, cardInfo.typeLine)
-    .print(
-      font,
-      82,
-      717,
-      { text: cardInfo.oracleText ? cardInfo.oracleText : "" },
-      640
-    )
-    .print(
-      font,
-      82,
-      1026,
-      {
-        text:
-          cardInfo.power && cardInfo.toughness
-            ? `${cardInfo.power} / ${cardInfo.toughness}`
-            : "",
-        alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT
-      },
-      640
-    )
-    .resize(227, Jimp.AUTO)
-    .write(cardInfo.imagePath, e => {
-      if (e) {
-        console.error(chalk.red.bold(e.message));
-        process.exit(1);
-      }
-      return;
-    });
-
-  return cardInfo;
+  return new Promise((resolve, reject) => {
+    image
+      .print(font, 82, 77, cardInfo.fullName)
+      .print(
+        font,
+        82,
+        77,
+        {
+          text: cardInfo.manaCost ? cardInfo.manaCost : "",
+          alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT
+        },
+        640
+      )
+      .print(font, 82, 642, cardInfo.typeLine)
+      .print(
+        font,
+        82,
+        717,
+        { text: cardInfo.oracleText ? cardInfo.oracleText : "" },
+        640
+      )
+      .print(
+        font,
+        82,
+        1026,
+        {
+          text:
+            cardInfo.power && cardInfo.toughness
+              ? `${cardInfo.power} / ${cardInfo.toughness}`
+              : "",
+          alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT
+        },
+        640
+      )
+      .resize(227, Jimp.AUTO)
+      .write(cardInfo.imagePath, e => {
+        if (e) {
+          reject(e);
+        }
+        resolve(cardInfo);
+      });
+  });
 }
 
 async function fetchAllCards(options) {
@@ -129,12 +129,86 @@ async function fetchAllCards(options) {
 
       return {
         title: cardName,
-        task: () => fetchCard({ quantity, cardName }, options)
+        task: () =>
+          fetchCard({ quantity, cardName }, options).then(cardInfo =>
+            options.cardsList.push(cardInfo)
+          )
       };
     }
   });
 
   return new Listr(tasks, { concurrent: true });
+}
+
+async function createPDF(options) {
+  const { exportPath, cardsList } = options;
+
+  const cardsHtml = cardsList.reduce((allCards, card) => {
+    const image = fs.readFileSync(card.imagePath);
+    const base64image = image.toString("base64");
+
+    const imageURI = `data:image/png;base64,${base64image}`;
+
+    const base = `<img class="card" src="${imageURI}" />`;
+
+    for (let i = 0; i < card.quantity; i++) {
+      allCards.push(base);
+    }
+
+    return allCards;
+  }, []);
+
+  const resultHtml = `<!DOCTYPE html>
+      <html>
+      <head>
+      <meta name=viewport content="width=device-width, initial-scale=1">
+      <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      #grid {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-around;
+      }
+
+      .card {
+        padding: 1.75em 0;
+      }
+      </style>
+      </head>
+      <body>
+      <div id="grid">
+      ${cardsHtml.join("\n")}
+      </div>
+      </body>
+      </html>
+      `;
+
+  const html5ToPDF = new HTML5ToPDF({
+    inputBody: resultHtml,
+    outputPath: path.join(exportPath, "output.pdf"),
+    options: {
+      pageSize: "A4",
+      marginsType: 1
+    }
+  });
+
+  await html5ToPDF.start();
+  await html5ToPDF.build();
+  await html5ToPDF.close();
+}
+
+async function cleanGarbage(options) {
+  const { cardsList } = options;
+
+  cardsList.map(({ imagePath }) => fs.unlinkSync(imagePath));
 }
 
 export default async function(options) {
@@ -148,7 +222,9 @@ export default async function(options) {
   const tasks = new Listr([
     { title: "Checking Card List", task: () => checkCardList(options) },
     { title: "Checking Export Path", task: () => checkExportPath(options) },
-    { title: "Fetching Cards", task: () => fetchAllCards(options) }
+    { title: "Fetching Cards", task: () => fetchAllCards(options) },
+    { title: "Creating PDF", task: () => createPDF(options) },
+    { title: "Cleaning download files", task: () => cleanGarbage(options) }
   ]);
 
   await tasks.run();
